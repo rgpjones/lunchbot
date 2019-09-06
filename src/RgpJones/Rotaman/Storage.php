b4c492a168cf1b2821491f1ae72617e3307abe6e
Rupert Jones <rupert.jones@inviqa.com>
2019-09-06 14:11:54 +0100

    Fix issue with Storage only working with an existing file

--- src/RgpJones/Rotaman/Storage.php
+++ src/RgpJones/Rotaman/Storage.php
@@ -4,29 +4,46 @@ namespace RgpJones\Rotaman;
 
 class Storage
 {
-    protected $file;
+    const STORAGE_FILE_PATH = '../../../var';
+
+    private $channel;
+
+    private $file;
 
     public function __construct($channel)
     {
-        $file = __DIR__.'/../../../var/'.$channel.'.json';
-        if (!file_exists($file) || !is_writable($file)) {
-            throw new \InvalidArgumentException('Invalid file');
+        $file = $this->getStorageFilename($channel);
+
+        if (!$this->fileIsWritable($file)) {
+            throw new \InvalidArgumentException(
+                sprintf('The storage file or containing directory for #%s cannot be written to.', $channel)
+            );
         }
+
+        $this->channel = $channel;
         $this->file = $file;
     }
 
     public function load()
     {
-        $data = [];
-        if (file_exists($this->file)) {
-            $data = json_decode(file_get_contents($this->file), true);
-        }
-
-        return $data;
+        return (file_exists($this->file))
+            ? json_decode(file_get_contents($this->file), true)
+            : [];
     }
 
     public function save($data)
     {
         file_put_contents($this->file, json_encode($data));
+    }
+
+    protected function getStorageFilename($channel): string
+    {
+        return sprintf('%s/%s/%s.json', __DIR__, self::STORAGE_FILE_PATH, $channel);
+    }
+
+    protected function fileIsWritable(string $file): bool
+    {
+        return (file_exists($file) && is_writable($file))
+            || is_writable(dirname($file));
     }
 }
